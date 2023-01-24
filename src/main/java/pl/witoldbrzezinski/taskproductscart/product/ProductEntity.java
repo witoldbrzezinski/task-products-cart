bf57225455bdc3a9864e1f5d92dc5c5b2e4646cf
Witold Brzezinski <witbrzezinski@gmail.com>
2023-01-24 16:26:32 +0100

    added flyway and product entity

---
+++ src/main/java/pl/witoldbrzezinski/taskproductscart/product/ProductEntity.java
@@ -0,0 +1,55 @@
+package pl.witoldbrzezinski.taskproductscart.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="products")
+@Where(clause = "is_deleted = false")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    private boolean isDeleted;
+    private final String uuid = UUID.randomUUID().toString();
+    @Version
+    private Long version;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductEntity that = (ProductEntity) o;
+        return Objects.equals(uuid, that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+}
